a692ecc22e0667af0eb6a663a4227206a780fe17
BlackPig95 <thisisblackpig@gmail.com>
2023-10-07 19:37:24 +0700

    Checking bug

--- Assets/00Game/01Script/PlayerController.cs
+++ Assets/00Game/01Script/PlayerController.cs
@@ -166,13 +166,14 @@ public class PlayerController : MonoBehaviour, ICanGetHit
             return;
         }
     }
-    private void OnCollisionStay2D(Collision2D _collision) //Collision Enter will cause bug when in corner
+    private void OnCollisionStay2D(Collision2D _collision) //Collision Enter will cause bug can't reset isGrounded
     {
-        GroundCheck();
+      // GroundCheck();
     }
     private void OnCollisionEnter2D(Collision2D _collision)
     {
         DetectEnemy();
+        GroundCheck();
     }
     void DetectEnemy()
     {
